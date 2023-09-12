dca0413a528a6d61ad2cf44b27040b79c7f8aa31
Piotr Markiewicz <piotr.markiewicz@gmail.com>
2023-09-12 15:53:26 +0200

    srt support

--- osd/frame.py
+++ osd/frame.py
@@ -7,3 +7,21 @@ class Frame:
     next_idx: int
     size: int
     data: bytes
+
+@dataclass
+class SrtFrame:
+    start_time: float
+    idx: int
+    signal: int
+    ch: int
+    flighttime: int
+    delay: int
+    bitrate: float
+    distance: int = 0
+    sbat: float = 0
+    gbat: float = 0
+    uavbat: float = 0
+    glsbat: float = 0
+    uavbatcells: int = 0
+    glsbatcells: int = 0
+    rcsignal: int = 0
